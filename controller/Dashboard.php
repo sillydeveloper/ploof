072042b7079fcf9efc29a51d83fce29c78b3d45e
frugalprogrammer <frugalprogrammer@d625bbe8-7b9a-11de-a624-ab7c8f4ec817>
2009-08-08 03:10:55 +0000

    Fix bad include

--- controller/Dashboard.php
+++ controller/Dashboard.php
@@ -5,9 +5,6 @@ class Dashboard extends core\Controller
     function index()
     {
         $this->assign("contact_email", "andrew@frugalprogrammer.com");
-        
-        $c= new Customer(2);
-        $this->assign("customer", $c);
     }   
     
     static function classname()
