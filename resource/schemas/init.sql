f28e202f4528b8c42e52da77f079622ef389d1f0
frugalprogrammer <frugalprogrammer@d625bbe8-7b9a-11de-a624-ab7c8f4ec817>
2009-09-07 17:43:06 +0000

    Add migrations

--- resource/schemas/init.sql
+++ resource/schemas/init.sql
@@ -1,5 +1,3 @@
 
-create table versioning(id int not null primary key auto_increment, migration int);
-
-insert into versioning values(null, 0);
+create table Versioning(id int not null primary key auto_increment, migration int, branch varchar(255));
     
