a34cf4c8669becf3b762d5a28463e6199c629e61
Nick Sinopoli <NSinopoli@gmail.com>
2011-01-25 19:43:34 -0800

    Implemented AbstractCache properly.

--- core/AbstractCache.php
+++ core/AbstractCache.php
@@ -8,31 +8,18 @@ abstract class AbstractCache
 
     abstract public function add($key, $value, $expiration=0);
 
-    abstract public function add_server($host, $port, $weight=0);
-
-    abstract public function append($key, $value);
-
-    abstract public function cas($token, $key, $value, $expiration=0);
-
-    abstract public function decrement($key, $offset=1);
-
     abstract public function delete($key, $time=0);
 
     abstract public function flush($delay=0);
 
     abstract public function get($key, $cache_callback=null, $cas_token=null);
 
-    abstract public function get_server_list();
-
-    abstract public function get_stats();
-
-    abstract public function increment($key, $offset=1);
-
-    abstract public function prepend($key, $value);
+    abstract public function key_exists($key);
 
     abstract public function replace($key, $value, $expiration=0);
 
     abstract public function set($key, $value, $expiration=0);
     
 }
+
 ?>
