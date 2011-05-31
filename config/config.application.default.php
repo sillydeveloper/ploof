b432c6536ac8c43d98eb1b8610d868649973b79e
Andrew Ettinger <sillydeveloper@gmail.com>
2011-05-31 12:33:51 -0700

    rst readme, installer for ploof, defaults for config.application.default.php

--- config/config.application.default.php
+++ config/config.application.default.php
@@ -1,4 +1,7 @@
 <?
 ### put your application specific configuration stuff here!
-
+$db= new \plugins\DB\MysqliConnector('my_username', 'my_password', 'my_database', 'my_server');
+$cache= new \plugins\cache\SessionCache();
+$repository= new \core\Repository($db, $cache);
+ApplicationModel::set_repository($repository);
 ?>
